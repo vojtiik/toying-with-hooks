43432d7ba8a9674f51c39d0aba0374299874e398
Vojtiik <Vojtech.Mares@justgiving.com>
2019-09-21 13:48:28 +0100

    setstate + custom

--- toying-with-hooks/src/App.js
+++ toying-with-hooks/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const name = useFormInput("Name");
+  const surname = useFormInput("Surname");
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="page">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>click me!</button>
+      </div>
+      <div className="page">
+        <p>{name.value + " " + surname.value}</p>
+        <section>
+          <div label="Name">
+            <input {...name} />
+          </div>
+          <div label="Surname">
+            <input {...surname} />
+          </div>
+        </section>
+      </div>
     </div>
   );
+}
+
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return {
+    value,
+    onChange: handleChange
+  };
 }
 
 export default App;
